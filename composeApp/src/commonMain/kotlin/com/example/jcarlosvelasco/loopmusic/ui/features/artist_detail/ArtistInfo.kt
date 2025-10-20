04ca4adb791164d2f2c80c6c384ba77bf9caa719
Juan Carlos Velasco <jcarlosvelasco14@gmail.com>
2025-10-20 17:08:29 +0200

    Removes imports

--- composeApp/src/commonMain/kotlin/com/example/jcarlosvelasco/loopmusic/ui/features/artist_detail/ArtistInfo.kt
+++ composeApp/src/commonMain/kotlin/com/example/jcarlosvelasco/loopmusic/ui/features/artist_detail/ArtistInfo.kt
@@ -1,10 +1,8 @@
 package com.example.jcarlosvelasco.loopmusic.ui.features.artist_detail
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
